a762336712204aa2b9fc957e2a802981e376f8d6
Joel <2016014076@unfv.edu.pe>
2023-04-27 23:20:42 -0500

    Add ratings field in BookSerializer

--- apps/books/api/serializers.py
+++ apps/books/api/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 from ..models import Book
-
+from apps.books.rating.api.serializers import RatingSerializer
 
 class BookSerializer(serializers.ModelSerializer):
+    # Adjunta el rating de libros
+    ratings = RatingSerializer(many=True, read_only=True, source='ratings_set')
+
     class Meta:
         model = Book
-        fields = "__all__"
+        fields = ["id","title","author","publisher","image_1","image_2","image_3","ratings"]
 
 class BookTitleUpdateSerializer(serializers.ModelSerializer):
     class Meta:
