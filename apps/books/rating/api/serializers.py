fa85a1e8dadcc465061fd382c2cec20317ee400a
Joel <2016014076@unfv.edu.pe>
2023-04-30 17:19:09 -0500

    Add rating detail serializer

--- apps/books/rating/api/serializers.py
+++ apps/books/rating/api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from apps.books.models import Rating
+from apps.users.api.serializers import UserSerializer
 
 
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
         fields = "__all__"
+
+
+class RatingDetailSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Rating
+        fields = ["user", "book"]
