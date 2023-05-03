6e8eed017c59962ad257eeea437e7c25a33d792f
Joel <2016014076@unfv.edu.pe>
2023-05-03 01:23:02 -0500

    Add num pages in response

--- utils/pagination.py
+++ utils/pagination.py
@@ -6,3 +6,9 @@ class CustomPagination(PageNumberPagination):
     page_query_param = "page"
     page_size_query_param = "page_size"
     max_page_size = 100
+
+    # Sobreescribir método para adjuntar n°de páginas en el response
+    def get_paginated_response(self, data):
+        response = super().get_paginated_response(data)
+        response.data["pages"] = self.page.paginator.num_pages
+        return response
